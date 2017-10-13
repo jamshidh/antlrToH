f381cee206f0c95b5b8bd9df734f6afe2ae50d2a
Jamshid <jamshidnh@gmail.com>
2017-10-13 17:51:30 -0400

    Making the input grammar be a parameter.

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,25 @@
 module Main where
 
+import Data.List
+import System.Environment
 import Text.Parsec
 
 import GrammarParser
 import ParserCreator
 
+usage::String
+usage = "antlrToH [filename]"
+  
+
 main :: IO ()
 main = do
-  --contents <- readFile "arithmetic.g4"
-  contents <- readFile "Solidity.g4"
+  args <- getArgs
+  contents <-
+    case filter (not . ("-" `isPrefixOf`)) args of
+     [] -> getContents
+     [filename] -> readFile filename
+     _ -> error $ unlines ["Incorrect usage:", usage]
+     
   let x = parse parseGrammar "<hardcoded>" contents
   case x of
    Left e -> putStrLn $ show e
